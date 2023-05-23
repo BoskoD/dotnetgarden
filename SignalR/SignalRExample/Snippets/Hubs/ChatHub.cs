2b57fd8e406c622528ddef4f5a6b0ac4ee386016
Bosko Danilovic <boskodanilovic@outlook.com>
2023-05-23 14:50:05 +0200

    SignalR example - Simple Chat app

---
+++ SignalR/SignalRExample/Snippets/Hubs/ChatHub.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRExample.Snippets.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "SignalR Users");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            await base.OnDisconnectedAsync(exception);
+        }
+
+        // Clients
+        public async Task SendMessage(string user, string message)
+        => await Clients.All.SendAsync("ReceiveMessage", user, message);
+
+        public async Task SendMessageToCaller(string user, string message)
+            => await Clients.Caller.SendAsync("ReceiveMessage", user, message);
+
+        public async Task SendMessageToGroup(string user, string message)
+            => await Clients.Group("SignalR Users").SendAsync("ReceiveMessage", user, message);
+
+        [HubMethodName("SendMessageToUser")]
+        public async Task DirectMessage(string user, string message)
+        => await Clients.User(user).SendAsync("ReceiveMessage", user, message);
+
+        public Task ThrowException()
+        => throw new HubException("This error will be sent to the client!");
+    }
+}
---
+++ SignalR/SignalRExample/Snippets/Hubs/IChatClient.cs
@@ -0,0 +1,7 @@
+﻿namespace SignalRExample.Snippets.Hubs
+{
+    public interface IChatClient
+    {
+        Task ReceiveMessage(string user, string message);
+    }
+}
